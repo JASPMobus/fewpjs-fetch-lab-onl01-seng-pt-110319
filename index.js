13379bb39b027822ca2ee496ca3dbccf48e801d6
James Mobus <mobusj@live.com>
2020-06-19 20:00:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function fetchBooks() {
+<<<<<<< HEAD
   return fetch('https://anapioficeandfire.com/api/books')
   .then(function(response) {
   return response.json();
@@ -6,6 +7,12 @@ function fetchBooks() {
   .then(function(json){
     renderBooks(json)
   })
+=======
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  
+  renderBooks(resp);
+>>>>>>> 59bf932f0d2873830db70c924df293c94a514515
 }
 
 function renderBooks(books) {
